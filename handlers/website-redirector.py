7a54594ac1418988f15b0e5e0236d16d07d4a381
Tom McLaughlin <tom@cloudzero.com>
2017-12-28 13:26:11 -0500

    Add some logging for debugging

--- handlers/website-redirector.py
+++ handlers/website-redirector.py
@@ -1,12 +1,21 @@
 '''
 Return a redirect location set in the environment.
 '''
+
+import json
+import logging
 import os
+
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
+logging.root.setLevel(logging.getLevelName(log_level))
+_logger = logging.getLogger(__name__)
 
 REDIRECT_LOCATION = os.environ.get('REDIRECT_LOCATION')
 STATUS_CODE = int(os.environ.get('STATUS_CODE'))
 
 def handler(event, context):
+    _logger.debug('Event received: {}'.format(json.dumps(event)))
+
     path = event.get('path')
 
     location = REDIRECT_LOCATION + event.get('path', '/')
