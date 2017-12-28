a7f335190c105c13ba214dee57d39558b8d26dd7
Tom McLaughlin <tom@cloudzero.com>
2017-12-28 14:46:20 -0500

    Breakup handler into smaller pieces.

--- handlers/website-redirector.py
+++ handlers/website-redirector.py
@@ -13,17 +13,34 @@ _logger = logging.getLogger(__name__)
 REDIRECT_LOCATION = os.environ.get('REDIRECT_LOCATION')
 STATUS_CODE = int(os.environ.get('STATUS_CODE'))
 
-def handler(event, context):
-    _logger.debug('Event received: {}'.format(json.dumps(event)))
+def _get_location(redirect_location, path, parameters):
+    '''Return the full location path'''
+    location = redirect_location + path
+    if parameters:
+        location += '?' + '&'.join(parameters)
+    return location
 
-    path = event.get('path')
 
+def _get_path_from_event(event):
+    '''Get path from event'''
+    return event.get('path')
+
+
+def _get_query_parameters_from_event(event):
+    '''Get query parameters from event'''
     query_parameters = event.get('queryStringParameters') or {}
     query_parameter_list = []
     for k in query_parameters:
         query_parameter_list.append('{}={}'.format(k, query_parameters.get(k)))
+    return query_parameter_list
 
-    location = REDIRECT_LOCATION + event.get('path', '/') + '?' + '&'.join(query_parameter_list)
+
+def handler(event, context):
+    _logger.debug('Event received: {}'.format(json.dumps(event)))
+
+    path = _get_path_from_event(event)
+    query_parameters = _get_query_parameters_from_event(event)
+    location = _get_location(REDIRECT_LOCATION, path, query_parameters)
 
     # Need to get URL from request so I can return full path
     return {
